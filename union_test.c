e49584975a80bc412fd5961fd1e73825bf66ef4a
Louisiy <1274908157@qq.com>
2022-04-30 12:51:50 +0800

    a

---
+++ union_test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+typedef union {
+    float lemon;
+    int lime_pieces;
+}lemon_lime;
+
+typedef struct {
+    float tequila;
+    float cointreau;
+    lemon_lime citrus;
+}
+
+margarita;
+
+int main () {
+    margarita m = {2.0, 1.0, .citrus.lemon=2};
+    // margarita m = {2.0, 1.0, {2}};
+    printf
+    ("%2.1f measures of tequila\n%2.1f measures of cointreau\n%2.1f measures of juice\n", m.tequila, m.cointreau, m.citrus.lemon);
+    system("pause");
+    return 0;
+
+}
