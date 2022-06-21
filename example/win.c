df703cecde8f03768ad21f1e045576b62ac1b9cb
Louisiy <1274908157@qq.com>
2022-06-21 20:19:03 +0800

    win_try

---
+++ example/win.c
@@ -0,0 +1,7 @@
+#include<windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevIstance,LPSTR lpCmdLine, int nCmdShow)
+{
+    MessageBox(NULL,"Hello World!","First Window",MB_OK);
+    return 0;
+}
