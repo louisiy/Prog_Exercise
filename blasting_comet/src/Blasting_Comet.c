130a5847a3bd72e6bacd493be3673a136e73e0b1
Louisiy <1274908157@qq.com>
2022-06-29 19:35:55 +0800

    new

--- blasting_comet/src/Blasting_Comet.c
+++ blasting_comet/src/Blasting_Comet.c
@@ -2,9 +2,21 @@
 
 int main(){
     //现在搭个框架，命令行版本
+
+    int _mode;
+
     printf("Blasting Comet\n");
     printf("Let's blast the comets!\n");
-    printf("Please enter your parament to configure your game.");
+    printf("Please enter your parament to configure your game.\n");
+
+    printf("mode:_/b");
+
+    while(scanf("%d",&_mode) != EOF){
+
+        printf("Good to see you here.");
+    }
+
+
 
     getchar();
     return 0;
