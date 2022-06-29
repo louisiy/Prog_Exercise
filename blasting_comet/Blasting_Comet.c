57dfa8e9bb0a7f704a9d94cbf54daf25020cbef0
Louisiy <1274908157@qq.com>
2022-06-29 19:28:36 +0800

    main

---
+++ blasting_comet/Blasting_Comet.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    //现在搭个框架，命令行版本
+    printf("Blasting Comet\n");
+
+    pause();
+    return 0;
+}
--- blasting_comet/src/Blasting_Comet.c
+++ blasting_comet/src/Blasting_Comet.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main(){
-    
+    //现在搭个框架，命令行版本
+    printf("Blasting Comet\n");
+    printf("Let's blast the comets!\n");
+    printf("Please enter your parament to configure your game.");
 
+    getchar();
     return 0;
 }
