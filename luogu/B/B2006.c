60748e58bf685a6ff9e0321d2685be272ec7396f
louisiy <louisiy@qq.com>
2022-08-20 20:34:51 +0800

    ook

--- luogu/B/B2006.c
+++ luogu/B/B2006.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
-    int x,a,y,b;
-    scanf("%d%d%d%d",&x,&a,&y,&b);
+    double x,a,y,b;
+    scanf("%lf%lf%lf%lf",&x,&a,&y,&b);
     double z;
     z = (a*x-b*y)/(a-b);
     printf("%.2f",z);
