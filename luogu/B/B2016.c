cd3e5b6a7fd92eccfd40c19c7027b4e848f62e06
louisiy <louisiy@qq.com>
2022-12-01 21:16:07 +0800

    aborts

---
+++ luogu/B/B2016.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    long long x ;
+    scanf("%lld",&x);
+    printf("%lld",x);
+    return 0;
+}
---
+++ luogu/B/B2019.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    bool b;
+    scanf("%d",&a);
+    b = a;
+    a = b;
+    printf("%d",a);
+    return 0;
+}
